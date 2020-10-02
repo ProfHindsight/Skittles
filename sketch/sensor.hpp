6b9863833907777f2504a3fbfb5d3d04ea6f7b11
ProfHindsight <colton.t.p@gmail.com>
2020-10-01 21:29:27 -0500

    fixing getRaw bug

--- sketch/sensor.hpp
+++ sketch/sensor.hpp
@@ -32,9 +32,9 @@ public:
         return this->color;
     }
 
-    void getRaw(uint16_t &red, uint16_t &green, uint16_t &blue, uint16_t &white)
+    void getRaw(uint16_t *red, uint16_t *green, uint16_t *blue, uint16_t *white)
     {
-        this->tcs.getRawData(&red, &green, &blue, &white);
+        this->tcs.getRawData(red, green, blue, white);
     }
 
     Color& getLast()
